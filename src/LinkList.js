ac6c1f7f9dda1f436c23460417bf56a67fa0e261
Andres Outon <outon.x@gmail.com>
2018-09-03 14:13:46 -0500

    created linkList component

---
+++ src/LinkList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "./link";
+
+const LinkList = ({ links }) => {
+  return (
+    <div className="linkList">
+      {links.map((link, i) => {
+        return (
+          <Link
+            key={i}
+            text={links[i].text}
+            description={links[i].description}
+            repo={links[i].repo}
+            web={links[i].web}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default LinkList;
