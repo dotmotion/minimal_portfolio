056d1e8042a3f2e905732ceebe5b1f5b227feb1e
Andres Outon <outon.x@gmail.com>
2018-09-03 14:26:48 -0500

    link module can now populate rom data file

--- src/Link.js
+++ src/Link.js
@@ -1,23 +1,26 @@
 import React from "react";
+import Eye from "./Eye.svg";
+import Code from "./Code.svg";
 import "./Link.css";
 
-const Link = () => {
+const Link = ({ text, description, repo, web }) => {
   return (
     <div class="grid-container">
       <div class="text">
-        <h2>Budget App</h2>
+        <h2>{text}</h2>
       </div>
       <div class="description">
-        <p>
-          Simple Budget application made with Vainilla JavaScript, HTML5 and
-          CSS3
-        </p>
+        <p>{description}</p>
       </div>
       <div class="web">
-        <p>EYE</p>
+        <a href={web}>
+          <img src={Eye} className="eye logo" alt="Live App" />
+        </a>
       </div>
       <div class="repo">
-        <p>CODE</p>
+        <a href={repo}>
+          <img src={Code} className="code logo" alt="GH Repo" />
+        </a>
       </div>
     </div>
   );
