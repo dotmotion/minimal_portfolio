cf448ac8b0be2d9895e81367ed7e0688a58e927c
Andres Outon <outon.x@gmail.com>
2018-09-03 14:24:50 -0500

    added information into data file

--- src/projects.js
+++ src/projects.js
@@ -3,23 +3,24 @@ export const projects = [
     //Modern Portfolio
     id: 1,
     text: "Modern Portfolio",
-    description: "",
-    repo: "URL",
-    web: "URL"
+    description: "Portfolio website created with HTML5 SASS and JavaScript",
+    repo: "https://github.com/dotmotion/portfolio",
+    web: "http://outon.mx"
   },
   {
     //Budjet App
     id: 2,
     text: "Budget App",
-    description: "",
-    repo: "URL",
-    web: "URL"
+    description:
+      "Simple Budget application made with Vainilla JavaScript, HTML5 and CSS3",
+    repo: "https://github.com/dotmotion/BudgetApp",
+    web: "https://dotmotion.github.io/BudgetApp/"
   },
   {
     //Photographer Portfolio
     id: 3,
     text: "Photography Portfolio",
-    description: "",
+    description: "TBD",
     repo: "URL",
     web: "URL"
   }
