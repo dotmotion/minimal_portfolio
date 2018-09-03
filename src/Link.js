e18f198f00c676befa8aa2053135d0fe6cc9e632
Andres Outon <outon.x@gmail.com>
2018-09-03 15:30:13 -0500

    imported dark icons

--- src/Link.js
+++ src/Link.js
@@ -1,28 +1,30 @@
 import React from "react";
-import Eye from "./Eye.svg";
-import Code from "./Code.svg";
+import Eye from "./Eye-dark.svg";
+import Code from "./Code-dark.svg";
 import "./Link.css";
 
 const Link = ({ text, description, repo, web }) => {
   return (
-    <div class="grid-container">
-      <div class="text">
-        <h2>{text}</h2>
+    <li>
+      <div class="grid-container">
+        <div class="text">
+          <h2>{text}</h2>
+        </div>
+        <div class="description">
+          <p>{description}</p>
+        </div>
+        <div class="web">
+          <a href={web}>
+            <img src={Eye} className="eye logo" alt="Live App" />
+          </a>
+        </div>
+        <div class="repo">
+          <a href={repo}>
+            <img src={Code} className="code logo" alt="GH Repo" />
+          </a>
+        </div>
       </div>
-      <div class="description">
-        <p>{description}</p>
-      </div>
-      <div class="web">
-        <a href={web}>
-          <img src={Eye} className="eye logo" alt="Live App" />
-        </a>
-      </div>
-      <div class="repo">
-        <a href={repo}>
-          <img src={Code} className="code logo" alt="GH Repo" />
-        </a>
-      </div>
-    </div>
+    </li>
   );
 };
 
