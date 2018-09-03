fb39372cc5c532f027afabd9edc3f45de42f6b6d
Andres Outon <outon.x@gmail.com>
2018-09-02 19:27:26 -0500

    created data file with dummy data

---
+++ src/projects.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    //Modern Portfolio
+    id: 1,
+    text: "Modern Portfolio",
+    description: "",
+    repo: "URL",
+    web: "URL"
+  },
+  {
+    //Budjet App
+    id: 2,
+    text: "Budget App",
+    description: "",
+    repo: "URL",
+    web: "URL"
+  },
+  {
+    //Photographer Portfolio
+    id: 3,
+    text: "Photography Portfolio",
+    description: "",
+    repo: "URL",
+    web: "URL"
+  }
+];
