a2dc080491d260283251ea23091b36da41d37060
Andres Outon <outon.x@gmail.com>
2018-09-02 23:50:32 -0500

    created link component

---
+++ src/Link.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Link.css";
+
+const Link = () => {
+  return (
+    <div class="grid-container">
+      <div class="text">
+        <h2>Budget App</h2>
+      </div>
+      <div class="description">
+        <p>
+          Simple Budget application made with Vainilla JavaScript, HTML5 and
+          CSS3
+        </p>
+      </div>
+      <div class="web">
+        <p>EYE</p>
+      </div>
+      <div class="repo">
+        <p>CODE</p>
+      </div>
+    </div>
+  );
+};
+
+export default Link;
