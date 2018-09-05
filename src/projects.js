72aa04fa728a6a1891e35985faede7deec1bbc00
Andres Outon <outon.x@gmail.com>
2018-09-05 00:25:56 -0500

    edited descriptions of projects

--- src/projects.js
+++ src/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     id: 1,
     text: "Budget App",
     description:
-      "Simple Budget application made with Vainilla JavaScript, HTML5 and CSS3",
+      "Simple Budget application built with vanilla JavaScript, HTML5 and CSS3. The main objective of this project was to avoid using libraries like jQuery, and practice the basics of JavaScript.",
     repo: "https://github.com/dotmotion/BudgetApp",
     web: "https://dotmotion.github.io/BudgetApp/"
   },
@@ -13,7 +13,7 @@ export const projects = [
     id: 2,
     text: "Herbalizer",
     description:
-      "List of cards that can be filtered with a search bar. Built with React",
+      "List of cards that can be filtered with a search bar. First project using React. Managed to build individual components from a data file and render the appropriate objects when filtering",
     repo: "https://github.com/dotmotion/Herbalizer",
     web: "https://dotmotion.github.io/Herbalizer/"
   },
@@ -21,7 +21,8 @@ export const projects = [
     //Modern Portfolio
     id: 3,
     text: "Modern Portfolio",
-    description: "Portfolio website created with HTML5 SASS and JavaScript",
+    description:
+      "Portfolio website created with HTML5 SASS and JavaScript. The main objective of this project was to learn to use SASS. I used mixins, SASS variables and partial files to create the finall .css file",
     repo: "https://github.com/dotmotion/portfolio",
     web: "http://outon.mx"
   }
