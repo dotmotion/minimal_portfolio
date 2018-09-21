9a2eab1acfae83682657d633ec3e7ac94e2e8618
Andres Outon <outon.x@gmail.com>
2018-09-20 20:52:13 -0500

    separated footer as it's own component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 import "./css/App.css";
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Content />
+        <Footer />
       </div>
     );
   }
--- src/components/Content.js
+++ src/components/Content.js
@@ -6,56 +6,12 @@ import { projects } from "../projects";
 
 const Content = () => {
   return (
-    <footer>
+    <main>
       <Notifications options={{ bottom: "0" }} />
       <ul>
         <LinkList links={projects} />
       </ul>
-      <div className="inner contact">
-        <h4>Ways to contact me:</h4>
-        <nav className="social">
-          <ul className="inline info">
-            <li>
-              <a
-                target="blank"
-                className="contact-link"
-                href="https://twitter.com/half_liquid"
-              >
-                Twitter
-              </a>
-            </li>
-            <li>
-              <CopyToClipboard
-                text="outon.x@gmail.com"
-                onCopy={() => {
-                  let myColor = {
-                    background: "#16191b",
-                    text: "#e2e6e8"
-                  };
-                  notify.show(
-                    "Email copied to clipboard!",
-                    "custom",
-                    3000,
-                    myColor
-                  );
-                }}
-              >
-                <span className="email">Email</span>
-              </CopyToClipboard>
-            </li>
-            <li>
-              <a
-                target="blank"
-                className="contact-link"
-                href="http://outon.mx/CV_ENG.pdf"
-              >
-                Download CV
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </footer>
+    </main>
   );
 };
 
