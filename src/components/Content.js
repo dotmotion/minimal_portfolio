32d9be4bf06136ed19a00dea67dfea94c9a8ae47
Andres Outon <outon.x@gmail.com>
2018-09-22 22:41:15 -0500

    moved footer into contect component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Notifications from "react-notify-toast";
 import LinkList from "./LinkList";
+import Footer from "./Footer";
 import { projects } from "../projects";
 
 const Content = () => {
   return (
     <main>
       <Notifications options={{ bottom: "0" }} />
-      <ul>
-        <LinkList links={projects} />
-      </ul>
+      <LinkList links={projects} />
+      <Footer />
     </main>
   );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,49 +4,51 @@ import { notify } from "react-notify-toast";
 
 const Footer = () => {
   return (
-    <footer className="inner contact">
-      <h4>Ways to contact me:</h4>
-      <nav>
-        <ul className="inline info">
-          <li>
-            <a
-              target="blank"
-              className="contact-link"
-              href="https://twitter.com/half_liquid"
-            >
-              Twitter
-            </a>
-          </li>
-          <li>
-            <CopyToClipboard
-              text="outon.x@gmail.com"
-              onCopy={() => {
-                let myColor = {
-                  background: "#16191b",
-                  text: "#e2e6e8"
-                };
-                notify.show(
-                  "Email copied to clipboard!",
-                  "custom",
-                  3000,
-                  myColor
-                );
-              }}
-            >
-              <span className="email">Email</span>
-            </CopyToClipboard>
-          </li>
-          <li>
-            <a
-              target="blank"
-              className="contact-link"
-              href="http://outon.mx/CV_ENG.pdf"
-            >
-              Download CV
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <footer>
+      <div className="contact">
+        <h4>Ways to contact me:</h4>
+        <nav>
+          <ul className="inline info">
+            <li>
+              <a
+                target="blank"
+                className="contact-link"
+                href="https://twitter.com/half_liquid"
+              >
+                Twitter
+              </a>
+            </li>
+            <li>
+              <CopyToClipboard
+                text="outon.x@gmail.com"
+                onCopy={() => {
+                  let myColor = {
+                    background: "#16191b",
+                    text: "#e2e6e8"
+                  };
+                  notify.show(
+                    "Email copied to clipboard!",
+                    "custom",
+                    3000,
+                    myColor
+                  );
+                }}
+              >
+                <span className="email">Email</span>
+              </CopyToClipboard>
+            </li>
+            <li>
+              <a
+                target="blank"
+                className="contact-link"
+                href="http://outon.mx/CV_ENG.pdf"
+              >
+                Download CV
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   );
 };
