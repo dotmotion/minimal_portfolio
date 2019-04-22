d8bbcd6654c9cd0e42a7ebf66a20a97f3bd07708
dotmotion <outon.x@gmail.com>
2019-04-22 14:45:42 -0500

    updated classes and removed unused parts

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,35 +3,30 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <div className="inner">
-        <div className="profile">
-          <h2>Andrés Outón,</h2>
-          <h2 className="cursor typewriter">Self-taught Web Developer</h2>
-          <nav className="social">
-            <ul className="inline">
-              <li>
-                <a target="blank" href="https://github.com/dotmotion">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <a target="blank" href="https://codepen.io/dotmotion/">
-                  CodePen
-                </a>
-              </li>
-              <li>
-                <a target="blank" href="https://twitter.com/half_liquid">
-                  Twitter
-                </a>
-              </li>
-              {/* <li>
-                <a target="blank" href="http://dotmotion.outon.mx/">
-                  Blog
-                </a>
-              </li> */}
-            </ul>
-          </nav>
+      <div className="profile">
+        <h2>Andrés Outón,</h2>
+        <div className="typewriter">
+          <h2>Self-taught Web Developer</h2>
         </div>
+        <nav className="social">
+          <ul className="inline">
+            <li>
+              <a target="blank" href="https://github.com/dotmotion">
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a target="blank" href="https://codepen.io/dotmotion/">
+                CodePen
+              </a>
+            </li>
+            <li>
+              <a target="blank" href="https://twitter.com/half_liquid">
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
